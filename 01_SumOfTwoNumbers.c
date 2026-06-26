d3e3314917d8af10f86909691799775ea7c191a1
snehamariasaju <145758700+snehamariasaju@users.noreply.github.com>
2026-06-26 11:24:28 +0530

    Add Sum of Two Numbers program

---
+++ 01_SumOfTwoNumbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//Reading two nos and display the output in the form of sum of 2 and 3 is 5
+int main()
+{
+    int a,b,c;
+    printf("Enter 2 nos :");
+    scanf ("%d%d",&a,&b);
+    c=a+b;
+    printf("Sum of %d and %d is %d",a,b,c);
+    return 0;
+}
